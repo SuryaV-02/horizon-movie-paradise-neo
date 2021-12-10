60d22d8ac12c77dc01028a566597ed18301ee355
Surya Venkatesan <suryaedu71@gmail.com>
2021-12-10 07:31:09 +0530

    COMPLETED homeScreen

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8183,7 +8183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7101" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5639" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
