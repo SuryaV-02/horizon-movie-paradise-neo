b8a06789b923a823aaf52dd3c863814c5b97d359
Surya Venkatesan <suryaedu71@gmail.com>
2021-12-28 22:23:03 +0530

    Added Dynamic profile-image(s)

--- dist/profile_details.0b80a49f.js
+++ dist/profile_details.0b80a49f.js
@@ -123,6 +123,10 @@ $(document).ready(function () {
   var user_email = document.getElementById("user-email");
   user_name.innerHTML = localStorage.getItem("user_name");
   user_email.innerHTML = localStorage.getItem("user_email");
+  var number = Math.floor(Math.random() * 5);
+  var url = "../assets/def_profile" + number + ".png";
+  var src_array = ['https://firebasestorage.googleapis.com/v0/b/horizon-movie-paradise-2.appspot.com/o/def_profile1.png?alt=media&token=3446cb28-5683-4df6-aeb2-04ffbbe1c86c', 'https://firebasestorage.googleapis.com/v0/b/horizon-movie-paradise-2.appspot.com/o/def_profile2.png?alt=media&token=6da918f3-9bfb-498a-8a7b-221b91e7ed0f', 'https://firebasestorage.googleapis.com/v0/b/horizon-movie-paradise-2.appspot.com/o/def_profile3.png?alt=media&token=d9a1821e-7b3f-4a0e-8fb0-d87bd6e7437a', 'https://firebasestorage.googleapis.com/v0/b/horizon-movie-paradise-2.appspot.com/o/def_profile4.png?alt=media&token=711f1637-2f3f-4034-8a92-0e944d37464f', 'https://firebasestorage.googleapis.com/v0/b/horizon-movie-paradise-2.appspot.com/o/def_profile5.png?alt=media&token=8d3e5a7d-ea6c-4f33-aea4-3c18241c4f2c'];
+  $("#img_profile_image").attr("src", src_array[number]);
 });
 },{}],"C:/Users/SURYA/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -152,7 +156,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11387" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13118" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8219,7 +8219,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9727" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13118" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
